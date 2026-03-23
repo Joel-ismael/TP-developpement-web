bf96261db49a0683fe74d28625edfd413a7590e8
Joel-ismael <joelismaeldomguiatakam@gmail.com>
2026-03-23 19:21:30 +0100

    Add files via upload

--- article.js
+++ article.js
@@ -1,11 +1,17 @@
-class Article {
-    constructor(titre, contenu, auteur, categorie, tags) {
-        this.titre = titre;
-        this.contenu = contenu;
-        this.auteur = auteur;
-        this.date = new Date().toISOString().split('T')[0]; // Format YYYY-MM-DD [cite: 94]
-        this.categorie = categorie;
-        this.tags = tags || [];
-    }
-}
-module.exports = { Article };
+const express = require('express');
+const {
+    ajouterArticle,
+    getTousArticles,
+    getArticle,
+    modifierArticle,
+    supprimerArticle,
+    rechercherArticles
+} = require("../controller/article");
+
+const router = express.Router();
+
+router.route("/articles/search").get(rechercherArticles); // Route de recherche [cite: 89]
+router.route("/articles").post(ajouterArticle).get(getTousArticles);
+router.route("/articles/:id").get(getArticle).put(modifierArticle).delete(supprimerArticle);
+
+module.exports = router;
